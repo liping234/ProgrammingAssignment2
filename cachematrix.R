5316c11ca405a405ff2258f910d617d1af669bed
ping <liping2009213939@163.com>
2016-06-01 22:05:16 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## set \ get is about x , but getinversion setinversion is about invs
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invs <- NULL
+    set <- function(y) {
+        x <<- y
+        invs <<- NULL
+    }
+    get <- function() x
+    setinversion <- function(inversion) invs <<- inversion
+    getinversion <- function() invs
+    list(set = set, get = get,
+         setinversion = setinversion,
+         getinversion = getinversion)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    invs <- x$getinversion()
+    if(!is.null(invs)) {
+        message("getting cached data")
+        return(invs)
+    }
+    data <- x$get()
+    invs <- solve(data)
+    x$setinversion(invs)
+    invs
 }
